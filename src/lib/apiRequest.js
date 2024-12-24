6afe75cf244b63a0ab7ec7feb2137a386617d6e0
sahil kulkarni <sahilkulkarni@Sahils-MacBook-Air-4.local>
2024-12-24 13:19:36 +0530

    new commit3

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -7,7 +7,7 @@ const apiRequest = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://localhost:8800/api"
-      : "https://campusbackend-v4vp.onrender.com",
+      : "https://campusbackend-v4vp.onrender.com/api",
   withCredentials: true,
 });
 
