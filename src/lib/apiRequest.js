45f1183c3c953fbdfb8eb937ec9a60f192bcbb6e
sahil kulkarni <sahilkulkarni@Sahils-MacBook-Air-4.local>
2024-12-30 23:57:53 +0530

    New1

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   baseURL:
-    window.location.hostname === "localhost123"
+    window.location.hostname === "localhost"
       ? "http://localhost:8800/api"
       : "https://campusbackend-v4vp.onrender.com/api",
   withCredentials: true,
